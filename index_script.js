d815dcb9bfff6a0dd8c8bb43127c5d903f5d9547
Kauê Brasil <kaue.malheiros@gmail.com>
2020-11-18 00:27:04 -0300

    mato 30

--- index_script.js
+++ index_script.js
@@ -4,8 +4,16 @@ window.onscroll = function () {
     if (currentScrollPos >= 1082) {
         document.getElementById("menu").style.visibility = 'visible';
         document.getElementById("menu").style.opacity = 1;
+
+        document.getElementById("stickhome_img").style.opacity = 1;
+        document.getElementById("stickhome_img").style.visibility = 'visible';
+        document.getElementById("stickhome_img").style.top = '40em';
     } else {
         document.getElementById("menu").style.visibility = 'hidden';
         document.getElementById("menu").style.opacity = 0;
+
+        document.getElementById("stickhome_img").style.opacity = 0;
+        document.getElementById("stickhome_img").style.visibility = 'hidden';
+        document.getElementById("stickhome_img").style.top = '0em';
     }
 }
