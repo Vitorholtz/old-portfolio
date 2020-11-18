117914f83feb565e3dc83e015873a2038d1ea086
Kauê Brasil <kaue.malheiros@gmail.com>
2020-11-18 00:29:16 -0300

    mas nao ganho

--- index_script.js
+++ index_script.js
@@ -1,17 +1,21 @@
 window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
     console.log(currentScrollPos);
-    if (currentScrollPos >= 1082) {
+    let position = 1082;
+
+    if (currentScrollPos >= position) {
         document.getElementById("menu").style.visibility = 'visible';
         document.getElementById("menu").style.opacity = 1;
+    } else {
+        document.getElementById("menu").style.visibility = 'hidden';
+        document.getElementById("menu").style.opacity = 0;
+    }
 
+    if (currentScrollPos >= position) {
         document.getElementById("stickhome_img").style.opacity = 1;
         document.getElementById("stickhome_img").style.visibility = 'visible';
         document.getElementById("stickhome_img").style.top = '40em';
     } else {
-        document.getElementById("menu").style.visibility = 'hidden';
-        document.getElementById("menu").style.opacity = 0;
-
         document.getElementById("stickhome_img").style.opacity = 0;
         document.getElementById("stickhome_img").style.visibility = 'hidden';
         document.getElementById("stickhome_img").style.top = '0em';
