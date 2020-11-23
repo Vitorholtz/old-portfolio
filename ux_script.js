2944230cd88922bfd79824b8cc8d21b3761b0db7
Vítor Holtz <vitorholtz27@gmail.com>
2020-11-23 16:32:51 -0300

    java

---
+++ ux_script.js
@@ -0,0 +1,15 @@
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    console.log(currentScrollPos);
+    let position = 700;
+
+    if (currentScrollPos >= position) {
+        document.getElementById("stickhome_img").style.opacity = 1;
+        document.getElementById("stickhome_img").style.visibility = 'visible';
+        document.getElementById("stickhome_img").style.top = '50%';
+    } else {
+        document.getElementById("stickhome_img").style.opacity = 0;
+        document.getElementById("stickhome_img").style.visibility = 'hidden';
+        document.getElementById("stickhome_img").style.top = '0em';
+    }
+}
